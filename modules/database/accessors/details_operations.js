437d3381b93e2c7ab6da677f9c13ac255c28daac
sahilayank <sahilayank91@gmail.com>
2020-07-31 19:50:56 +0530

    updated details_operations

--- modules/database/accessors/details_operations.js
+++ modules/database/accessors/details_operations.js
@@ -1,56 +1,93 @@
-var Details = require(__BASE__ + "modules/database/models/Details");
-var mongoose = require('mongoose');
-var LOGGER = require(__BASE__ + "modules/utils/Logger");
-var extend = require('util')._extend;
-var Promise = require('bluebird');
+const Details = require(`${__BASE__}modules/database/models/Details`);
+const Promise = require("bluebird");
 
-var getCreateTemplate = function (parameters) {
-
-    var template = {};
-    for (var key in parameters) {
-        switch (key) {
-            case 'user':
-            case 'age':
-            case 'monthlyIncome':
-            case 'gender':
-            case 'isMarried':
-            case 'isSpouseWorking':
-            case 'ageSpouse':
-            case 'numberOfKids':
-            case 'kids':
-            case 'disabilityPresent':
-            case 'isHavingCar':
-            case 'isHavingCarInsurance':
-            case 'isHavingHealthInsurance':
-            case 'isHavingLifeInsurance':
-            case 'isHavingTermInsurance':
-                template[key] = parameters[key];
-                break;
-        }
+const getCreateTemplate = function(parameters) {
+  const template = {};
+  for (const key in parameters) {
+    switch (key) {
+      case "user":
+      case "age":
+      case "monthlyIncome":
+      case "gender":
+      case "isMarried":
+      case "isSpouseWorking":
+      case "ageSpouse":
+      case "numberOfKids":
+      case "kids":
+      case "disabilityPresent":
+      case "isHavingCar":
+      case "isHavingCarInsurance":
+      case "isHavingHealthInsurance":
+      case "isHavingLifeInsurance":
+      case "isHavingTermInsurance":
+        template[key] = parameters[key];
+        break;
     }
+  }
 
-    template.create_time = new Date();
+  template.create_time = new Date();
 
-    return template;
-}
-
-let createDetail = function (user, parameters) {
-    return new Promise(function (resolve, reject) {
-        let template = getCreateTemplate(parameters);
-        let details = new Details(template);
-        details.save(function (err, data) {
-            if (!err) {
-                resolve(data)
-            } else {
-                //   LOGGER.logErrorMessage('CreateDetails', err, template);
-                reject(new Error('Failed to create Details for the user'));
-            }
-        });
-    });
+  return template;
 };
 
+const getUpdateTemplate = function(parameters) {
+  const template = {};
+  for (const key in parameters) {
+    switch (key) {
+      case "age":
+      case "monthlyIncome":
+      case "isMarried":
+      case "isSpouseWorking":
+      case "ageSpouse":
+      case "numberOfKids":
+      case "kids":
+      case "disabilityPresent":
+      case "isHavingCar":
+      case "isHavingCarInsurance":
+      case "isHavingHealthInsurance":
+      case "isHavingLifeInsurance":
+      case "isHavingTermInsurance":
+        template[key] = parameters[key];
+        break;
+    }
+  }
+
+  template.update_time = new Date();
+
+  return template;
+};
+
+const createDetail = function(user, parameters) {
+  return new Promise((resolve, reject) => {
+    const template = getCreateTemplate(parameters);
+    const details = new Details(template);
+    details.save((err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        //   LOGGER.logErrorMessage('CreateDetails', err, template);
+        reject(new Error("Failed to create Details for the user"));
+      }
+    });
+  });
+};
+
+const updateDetail = function(user, parameters) {
+  return new Promise((resolve, reject) => {
+    const template = getUpdateTemplate(parameters);
+    const details = new Details(template);
+    details.findOneAndUpdate({ user }, template, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        //   LOGGER.logErrorMessage('CreateDetails', err, template);
+        reject(new Error("Failed to create Details for the user"));
+      }
+    });
+  });
+};
 
 module.exports = {
-    createDetail: createDetail
-}
-
+  createDetail,
+  updateDetail
+};
