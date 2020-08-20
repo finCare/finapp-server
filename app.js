d5eca2a6fcbee94b51a463b3a207c26478ae0fec
sahilayank <sahilayank91@gmail.com>
2020-08-21 01:56:08 +0530

    udpate host

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ mongoose.connection.on("error", err => {
 /**
  * Express configuration.
  */
-app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
+
+app.set("host", process.env.HOST || "0.0.0.0");
 app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
